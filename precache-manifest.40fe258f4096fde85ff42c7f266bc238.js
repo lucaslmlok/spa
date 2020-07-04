34deb675ff8c656d597480f3901546ec349ae317
lucaslmlok <lucaslo@creasant.com>
2020-07-04 13:53:59 +0800

    Updates

---
+++ precache-manifest.40fe258f4096fde85ff42c7f266bc238.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b09eb2d13eaa10aa5440a7f11a238a9",
+    "url": "/spa-website/index.html"
+  },
+  {
+    "revision": "28955d5b0512adab6223",
+    "url": "/spa-website/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "ce524714445e9fb8872e",
+    "url": "/spa-website/static/css/main.2ab8d3cd.chunk.css"
+  },
+  {
+    "revision": "28955d5b0512adab6223",
+    "url": "/spa-website/static/js/2.b5e58b70.chunk.js"
+  },
+  {
+    "revision": "ce524714445e9fb8872e",
+    "url": "/spa-website/static/js/main.1c91490f.chunk.js"
+  },
+  {
+    "revision": "c2bfdb5b2ecc14b4ba5e",
+    "url": "/spa-website/static/js/runtime~main.beebbf7f.js"
+  },
+  {
+    "revision": "4bd9f03ca7a136b2f96fa4423fb407bf",
+    "url": "/spa-website/static/media/about1.4bd9f03c.jpeg"
+  },
+  {
+    "revision": "c6cc5b57a23e8e4da5d425f7893139d5",
+    "url": "/spa-website/static/media/about2.c6cc5b57.jpeg"
+  },
+  {
+    "revision": "f6f1a8ad87ef7bade710e74dbf581f16",
+    "url": "/spa-website/static/media/about3.f6f1a8ad.jpeg"
+  },
+  {
+    "revision": "748c734ac3de38465202cec654f441a8",
+    "url": "/spa-website/static/media/about4.748c734a.jpeg"
+  },
+  {
+    "revision": "d02390cbbe0e1d03dc56e540023de8a4",
+    "url": "/spa-website/static/media/abouthero.d02390cb.jpeg"
+  },
+  {
+    "revision": "6599d6d73dbdd24cf1cee091f54cafbd",
+    "url": "/spa-website/static/media/defaultBcg.6599d6d7.jpeg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/spa-website/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "76b850a831c34d50dd1477570a83d377",
+    "url": "/spa-website/static/media/roomshero.76b850a8.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spa/precache-manifest.6779018ecd7cae61f943fc86f605fdad.js"
+  "/spa-website/precache-manifest.40fe258f4096fde85ff42c7f266bc238.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spa/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spa-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
